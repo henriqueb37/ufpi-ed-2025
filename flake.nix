01679f866ce1c22acaf702e7692e173c135a33b9
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2025-10-02 01:18:56 -0300

    flakes adicionados

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = (import nixpkgs {
+        inherit system;
+      });
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ clang-tools cmake ];
+        buildInputs = with pkgs; [ gcc gdb valgrind ];
+        shellHook = ''
+          function run_cpp {
+            outDir="`dirname $(realpath $1)`/bin"
+            mkdir -p "$outDir"
+            g++ -std=gnu++17 -Wall -ggdb "$1" -o ''${outDir}/''${1%.cpp}.out
+            ''${outDir}/''${1%.cpp}.out
+          }
+        '';
+      };
+    };
+}
